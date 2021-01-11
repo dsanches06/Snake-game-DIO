99bf44a8787a3ea0f6794ab0ca66746c8f30d76f
Danilson Sanches <dsanches06@outlook.com>
2021-01-11 20:27:37 +0000

    definir movimentos da cobrinha

--- snake-game/js/main.js
+++ snake-game/js/main.js
@@ -3,10 +3,11 @@ let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
 snake[0] = {
-  //posição inicial da cobra
+  //coordenadas default da cobra
   x: 8 * box,
   y: 8 * box,
 };
+let direction = "right";
 
 function criarBG() {
   context.fillStyle = "lightgreen";
@@ -20,5 +21,32 @@ function criarSnake() {
   }
 }
 
-criarBG();
-criarSnake();
+function iniciarJogo() {
+  criarBG();
+  criarSnake();
+
+  /* criar posicao inicial da cobra  */
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+
+  /* criar as coordenadas da cobra para movimentar */
+  if (direction == "right") snakeX += box;
+  if (direction == "left") snakeX -= box;
+  if (direction == "up") snakeY -= box;
+  if (direction == "down") snakeY += box;
+
+  /* para retirar o ultimo elemento do array */
+  snake.pop();
+
+  /* criar um novo cabeçalho */
+  let newHead = {
+    x: snakeX,
+    y: snakeY,
+  };
+
+  /* acrescenta um elemento a frente do primeiro elemento */
+  snake.unshift(newHead);
+}
+
+/* iniciar o jogo */
+let jogo = setInterval(iniciarJogo, 100);
