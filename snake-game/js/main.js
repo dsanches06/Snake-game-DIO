4cedad475f2a609c2ddc5c12c16d357bbf8a9dd3
Danilson Sanches <dsanches06@outlook.com>
2021-01-11 20:56:17 +0000

    definir as direcões sem que cobrinha saia da zona do jogo

--- snake-game/js/main.js
+++ snake-game/js/main.js
@@ -1,9 +1,11 @@
+/* definir variaveis globais */
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
+
+/* coordenadas default da cobra */
 snake[0] = {
-  //coordenadas default da cobra
   x: 8 * box,
   y: 8 * box,
 };
@@ -14,6 +16,7 @@ function criarBG() {
   context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
+/* Função que cria a cobrinha */
 function criarSnake() {
   for (let i = 0; i < snake.length; i++) {
     context.fillStyle = "green";
@@ -21,7 +24,30 @@ function criarSnake() {
   }
 }
 
+/* criar eventos das teclas de direcção do teclado */
+document.addEventListener("keydown", update);
+
+/* Função para atualizar os eventos capturados do teclado */
+function update(event) {
+  //37 -> direita
+  if (event.keyCode == 37 && direction != "right") direction = "left";
+  //38 -> baixo
+  if (event.keyCode == 38 && direction != "down") direction = "up";
+  //39 -> esquerda
+  if (event.keyCode == 39 && direction != "left") direction = "right";
+  //40 -> cima
+  if (event.keyCode == 40 && direction != "up") direction = "down";
+}
+
+/* Função que inicia o jogo */
 function iniciarJogo() {
+  /* definir direcoes da cobra sem sair da canvas*/
+  if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+  if (snake[0].x < 0 * box && direction == "left") snake[0].x = 16 * box;
+  if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+  if (snake[0].y < 0 * box && direction == "up") snake[0].y = 16 * box;
+
+  /* chamar as funçoes inicias */
   criarBG();
   criarSnake();
 
@@ -48,5 +74,5 @@ function iniciarJogo() {
   snake.unshift(newHead);
 }
 
-/* iniciar o jogo */
+/* iniciar o jogo no intervalo de 1 segundo */
 let jogo = setInterval(iniciarJogo, 100);
